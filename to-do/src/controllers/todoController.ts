7ef0bd13ee6519680f7efb500857c47a42f5f025
Taha Zoabi <zoabi03@gmail.com>
2024-11-01 15:05:44 +0200

    post request to create a todo

--- to-do/src/controllers/todoController.ts
+++ to-do/src/controllers/todoController.ts
@@ -8,3 +8,30 @@ export const getTodos: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
+
+interface CreateTodoBody {
+  title?: string;
+  description?: string;
+}
+
+export const createTodo: RequestHandler<
+  unknown,
+  unknown,
+  CreateTodoBody,
+  unknown
+> = async (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  try {
+    if (!title) {
+      console.log("Note must have a title");
+    }
+    const newTodo = await TodoModel.create({
+      title,
+      description,
+    });
+    res.status(201).json(newTodo);
+  } catch (error) {
+    next(error);
+  }
+};
